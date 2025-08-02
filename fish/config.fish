5379ad29d214f5a289ef2ae2a0782ea7703a18a9
Keith994 <keithwoo1994@163.com>
2025-08-02 17:00:45 +0800

    v3.2.3 update config

--- fish/config.fish
+++ fish/config.fish
@@ -1,6 +1,6 @@
 if status is-interactive
     # Commands to run in interactive sessions can go here
-    jenv init - | source
+    jenv init - |sed '/hash -r/d' | psub| source
     starship init fish | source
     zoxide init fish | source
 end
